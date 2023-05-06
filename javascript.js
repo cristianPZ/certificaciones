e2b3d49fa3071ac1b44361c9ad51b97df7284014
cristianPZ <73722070+cristianPZ@users.noreply.github.com>
2023-05-06 20:16:50 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ document.getElementById("mi-boton").addEventListener("click", function(event) {
   // Esperar a que la animación termine antes de redirigir
   setTimeout(function() {
     // Redirigir al usuario al enlace deseado
-    window.location.href = "file:///C:/Users/perey/Documents/proyectos%20web/certificaciones/index.html";
+    window.location.href = "cristianpz.github.io";
   }, 1500); // 2500 ms = 2.5 segundos
 });
 
